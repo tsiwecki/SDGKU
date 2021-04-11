77877cd5d04dd836dd5d1df3e78162bed8cd407a
Thad Siwecki <80732159+tsiwecki@users.noreply.github.com>
2021-04-11 15:12:40 -0500

    Add files via upload

---
+++ FSDI 103 Assignment 1/script.js
@@ -0,0 +1,45 @@
+//(1) Create one script with 20 different variables and display them on the HTML.
+
+var firstName='selectFirstName';
+var lastName='selectUserLastName';
+var userName='selectUserNane';
+var userEmail='selectUserEmail';
+var userAge='selectUserAge';
+var userCountry='selectUserCountry';
+var userRelationshipStatus='userSelectRelationshipStatus';
+
+let first='Thad';
+let name='Siwecki';
+let user='tsiwecki12345';
+let email='thad.siwecki@gmail.com';
+let age=54;
+let country='united States of America';
+let status='married';
+
+document.writeln('Thad'); 
+document.writeln('Siwecki');
+document.writeln('tsiwecki12345');
+document.writeln('thad.siwecki@gmail.com');
+document.writeln(57);
+document.writeln('USA');
+document.writeln('Married');
+
+//(2) Create one script with a for loop and display the numbers from 1 to 100, 2 by 2.
+
+let count = 2;
+while (count <= 100) {
+    console.log(count);
+    count = count + 2;
+}
+
+alert("Warning! This message will self-destruct in");
+
+alert("3..."); 
+alert("2..."); 
+alert("1..."); 
+
+document.querySelector("h1").textContent = "🔥BOOM!   Message destroyed!  🔥";
+
+//(3) Create one script with an if statement.
+
+

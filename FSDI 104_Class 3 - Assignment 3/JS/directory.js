3bc63bacb720bae938142a38c4df57185387062b
Thad Siwecki <80732159+tsiwecki@users.noreply.github.com>
2021-04-23 22:49:14 -0500

    Add files via upload

---
+++ FSDI 104_Class 3 - Assignment 3/JS/directory.js
@@ -0,0 +1,35 @@
+function display(){
+    // travel the salon.pets
+    document.getElementById("pets").innerHTML="";
+    for(var i=0;i<salon.pets.length;i++){
+        var aPet= salon.pets[i]};
+    //creat a tmp
+        
+        var tmp =`
+        <div class="pet>
+            <h3> ${aPet.name} </h3>
+            <p> ${aPet.age}</p>
+            <p> ${aPet.gender}</p>
+            <p> ${aPet.breed}</p>
+            <p> ${aPet.color}</p>
+            <p> ${aPet.service}</p>
+            <p> ${aPet.ownerName}</p>
+            <p> ${aPet.ownerPhone}</p>
+
+
+        </div>`;
+        
+        if (aPet ==""){
+            alert("Field must be filled out");
+            return false;
+        }
+        
+        console.log(tmp);  
+        document.getElementById("pets").inneHTML+=tmp;
+
+    }      
+    //append thee tmp into the html
+
+   
+
+display();
---
+++ FSDI 104_Class 3 - Assignment 3/JS/register.js
@@ -0,0 +1,83 @@
+const salon={
+    name:"The Fashion Pets",
+    address:{
+        street:"University Ave.",
+        number:"12345",
+    },
+hours:{
+    open:"9:00 am",
+    close:"5:00 pm",
+},
+pets:[]
+}
+
+var{name,address:{street,number}}=salon;
+document.getElementById("footer-info").innerHTML=`
+    <p> ${name} ${street} ${number}</p>`;
+
+function displayText(){
+    for(var i=0;i<salon.pets.length;i++){
+        console.log(salon.pets[i].petName);
+    }
+}
+
+//create the object constructor
+class Pets{
+    constructor(name,age,gender,breed,service,ownerName,contactPhone){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.breed=breed;
+        this.color=color;
+        this.service=service;
+        this.ownerName=ownerName;
+        this.contactPhone-contactPhone;
+    }
+}
+
+
+//create  3 pets
+var pet1= new Pet("Scooby",50,"Male","Great Dane","Brown","Grooming","Shaggy","555-555-5555");
+var pet2= new Pet("Tiger",50,"Male","Mixed","Black/Brown","Full Service","Tim","555-555-5556");
+var pet2= new Pet("Tweety Bird",70,"Male","Cannary","Yellow","Nail Cut","Bugs Bunny","555-555-5557");
+salon.pets.push(pet1);
+salon.pets.push(pet2);
+salon.pets.push(pet3);
+
+//create the register function
+
+// read the values from the inputs and store the information into vars
+var inputName = document.getElementByID("petName");
+var inputAge = document.getElementByID("petAge");
+var inputGender = document.getElementById("petGender");
+var inputBreed = documenrt.getElementByID("petBreed");
+var inputColor = documenrt.getElementByID("petColor");
+var inputService = document.getElementByID("petService");
+var inputOwner = document.getElementByID("ownerName");
+var inputPhone = document.getElementByID("ownerPhone");
+
+function register(){
+    console.log(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputColor.value,inputService.value,inputOwner.value,inputPhone.value);
+
+    // create a generic object and pass the info of the vars
+    var thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputColor.value,inputService.value,inputOwner.value,inputPhone.value);
+    console.log(thePet);
+
+    // push the object into the array
+    salon.pets.push(thePet);
+
+    // clear the input to improve the user inputs
+    clear();
+    display();
+}
+
+function clear() {
+    inputName.value="";
+    inputAge.value="";
+    inputGender.value="";
+    inputBreed.value="";
+    inputColor.value="";
+    inputService.value="";
+    inputOwner.value="";
+    inputPhone.value="";
+}

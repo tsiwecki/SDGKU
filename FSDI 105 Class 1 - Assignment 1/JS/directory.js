b862a17ade678ff92620d23dc34d83080ceb53a6
Thad Siwecki <80732159+tsiwecki@users.noreply.github.com>
2021-05-06 22:44:43 -0500

    Add files via upload

--- FSDI 105 Class 1 - Assignment 1/JS/directory.js
+++ FSDI 105 Class 1 - Assignment 1/JS/directory.js
@@ -1,8 +1,8 @@
 function display(){
     // travel the salon.pets
-    document.getElementById("pets").innerHTML="";
+    document.getElementById('pets').innerHTML="";
     for(var i=0;i<salon.pets.length;i++){
-        var aPet = salon.pets[i]};
+        var aPet= salon.pets[i];
 
         if(aPet.service==="full"){
             aPet.price=30;
@@ -14,26 +14,28 @@ function display(){
             aPet.price=20;
         }
 
-        // create the row
-        var row=document.createElement('tr');
-        row.innerHTML=`
-        <div id="${aPet.id}"class="pets">
-                <h3> ${aPet.name} </h3>
-                <p> ${aPet.age} </p>
-                <p> ${aPet.gender} </p>
-                <p> ${aPet.breed} </p>
-                <p> ${aPet.color} </p>
-                <p> ${aPet.service} </p>
-                <p> ${aPet.price} </p>
-                <p> ${aPet.ownerName} </p>
-                <p> ${aPet.contactPhone} </p>
+     // create the tmp
+        var tmp =`
+        <div id="${aPet.id}" class="pet">
+                <td><h3> ${aPet.name} </h3></td>
+                <td><p> ${aPet.age} </p></td>
+                <td><p> ${aPet.gender} </p></td>
+                <td><p> ${aPet.type} </p></td>
+                <td><p> ${aPet.breed} </p></td>
+                <td><p> ${aPet.color} </p></td>
+                <td><p> ${aPet.service} </p></td>
+                <td><p> ${aPet.price} </p></td>
+                <td><p> ${aPet.ownerName} </p></td>
+                <td><p> ${aPet.contactPhone} </p></td>
+                <td><p> ${aPet.email} </p></td>
                 <button onclick="deletePet(${aPet.id})" class="btn btn-danger"> Delete </button>
             <div>`;
             console.log(tmp);
-            document.getElementById("pets");innerHtml+=tmp;
+            document.getElementById('pet');innerHtml+=tmp;
         
-            updateProfits();
         }
+            updateProfits();
+    } 
 
         function displayTable(aPet){
             if(aPet.service==="full"){
@@ -46,47 +48,40 @@ function display(){
                 aPet.price=20;
             }
             var icon="";
-            if(aPet.type==="dog"){ icon="🐕";
+            if(aPet.type==="dog"){ 
+                icon="🐕";
             }else if(aPet.type==="cat"){
                 icon="🐈";
-            }else if(aPet.tyoe==="bird"){
+            }else if(aPet.type==="bird"){
                 icon="🐦";
             }else{
                 icon="🐾";
             }
         
 
-
-        //append the row to a table
-        document.querySelector("aPet").appendChild(row);
-        updateProfits();
-    }
-        //creat a tmp
-        
+        // create the row
         var row=document.createElement('tr');
         row.innerHTML=`
-        <div id="${aPet.id}" class="pets">
-            <h3> ${aPet.name} </h3>
-            <p> ${aPet.age}</p>
-            <p> ${aPet.gender}</p>
-            <p> ${aPet.breed}</p>
-            <p> ${aPet.color}</p>
-            <p> ${aPet.service}</p>
-            <p> ${aPet.ownerName}</p>
-            <p> ${aPet.ownerPhone}</p>
-        </div>`;
-        
-        
-        if (aPet ==""){
-            alert("Field must be filled out");
-        }
-        
-        console.log(tmp);  
-        document.getElementById("pets").innerHTML+=tmp;
+            <td> ${aPet.name} </td>
+            <td> ${aPet.age} </td>
+            <td> ${aPet.gender} </td>
+            <td> ${icon}</td>
+            <td> ${aPet.type} </td>
+            <td> ${aPet.breed} </td>
+            <td> ${aPet.color} </td>
+            <td> ${aPet.service} </td>
+            <td> ${aPet.ownerName} </td>
+            <td> ${aPet.contactPhone} </td>
+            <td> ${aPet.email} </td>    
+            <td> ${aPet.price} </td>
+        `;
 
+        //append the row to a table
+        document.querySelector('#petTable').appendChild(row);
         updateProfits();
+    }
+        
 
-        generateTable("petTable");
 
         
 
--- FSDI 105 Class 1 - Assignment 1/JS/register.js
+++ FSDI 105 Class 1 - Assignment 1/JS/register.js
@@ -9,23 +9,24 @@ const salon={
         close:"Closes at: 5:00 pm",
         days:"Monday-Friday",
     },
-pets:[],
+pets:[]
 }
 
-var {name,address:{number,street},hours:{open,close,days}}=salon;
-document.getElementById("footer-info").innerHTML=`
+var{name,address:{number,street},hours:{open,close,days}}=salon;// object deconstructing
+
+document.getElementById('footer-info').innerHTML=`
     <p> ${name}</p>  <p> ${number}  ${street} </p>  <p> ${open} ${close} </P>  <p> ${days}`;
 
 function displayText(){
     for(var i=0;i<salon.pets.length;i++){
-        displayTable(salon.pets[i].petName);
+        console.log(salon.pets[i].petName);
     }
 }
 var counter=0;
 
 //create the object constructor
 class Pet{
-    constructor(name,age,gender,type,breed,color,service,ownerName,contactPhone,email,price){
+    constructor(name,age,gender,type,breed,color,service,ownerName,contactPhone,email){
         this.name=name;
         this.age=age;
         this.gender=gender;
@@ -35,59 +36,52 @@ class Pet{
         this.service=service;
         this.ownerName=ownerName;
         this.contactPhone-contactPhone;
-        this.email = email;
+        this.contactPhone-email;
         this.price=0;
         this.id=counter++;
     }
 }
 
+
+
+
+//create the register function
 // read the values from the inputs and store the information into vars
-var inputName = document.getElementById("petName");
-var inputAge = document.getElementById("petAge");
-var inputGender = document.getElementById("petGender");
-var inputType = document.getElementById("petType");
-var inputBreed = document.getElementById("petBreed");
-var inputColor = document.getElementById("petColor");
-var inputService = document.getElementById("petService");
-var inputOwner = document.getElementById("ownerName");
-var inputPhone = document.getElementById("ownerPhone");
-var inputEmail = document.getElementById("petEmail").value;
+var inputName = $('#petName');
+var inputAge = $('#petAge')
+var inputGender = $('#petGender');
+var inputType = $('#petType');
+var inputBreed = $('#petBreed');
+var inputColor = $('#petColor');
+var inputService = $('#petService');
+var inputOwner = $('#ownerName');
+var inputPhone = $('#email');
 
 
- // create a generic object and pass the info of the vars
 function register(){
-       
-    var thePet = new Pet(inputName.value,
-        inputAge.value,
-        inputGender.value,
-        inputBreed.value,
-        inputColor.value,
-        inputService.value,
-        inputOwner.value,
-        inputPhone.value,
-        imputEmail.value);
     
-        var price;
-        if (inputService === "Wash") {
-            price = salon.prices.wash;
-        } else if (inputService === "Groom") {
-            price = salon.prices.groom;
-        } else if (inputService === "Full Service") {
-            price = salon.prices.fullService;
-        }
-    
-        var newPet = new Pet(petId, inputName, inputAge, inputGender, inputType, inputBreed, inputService, inputOwner, inputPhone, inputEmail, price);
-    
-        salon.pets.push(newPet);
-}
-    
+// create a generic object and pass the info of the vars
+    var thePet = new Pet(
+        $('#petName').val(),
+        $('#petAge').val(),
+        $('#petGender').val(),
+        $('#petType').val(),
+        $('#petBreed').val(),
+        $('#petColor').val(),
+        $('#petService').val(),
+        $('#ownerName').val(),
+        $('#ownerPhone').val(),
+        $('#ownerEmail').val());
+    console.log(thePet);
 
     // push the object into the array
     salon.pets.push(thePet);
 
-    // clear the input to improve the user inputs
+    // clear the inputs
     clear();
-    displayTable();
+    displayTable(thePet);
+    display();
+}
 
 function clear() {
     inputName.val("");
@@ -99,84 +93,83 @@ function clear() {
     inputService.val("");
     inputOwner.val("");
     inputPhone.val("");
-    }    
+    inputEmail.val("");
+}
 
 function updateProfits(){
     var profit=0;
     for(var i=0;i<salon.pets.length;i++){
         profit=profit+salon.pets[i].price;
     }
-    document.getElementById('profits').innerHTML=`Profits = $${profits}.00`;
+    document.getElementById('profits').innerHTML=`Profits = $${profit}.00`;
 }
 
 function deletePet(petId){
-    //select the card with the pet
+//select the card with the pet
     var card=$('#'+petId);
-    displayTable('delete pet'+card);
-    //search the pet in the array
+    console.log('delete pet'+petId);
+//search the pet in the array
     var indexDelete;
     for(var i=0;i<salon.pets.length;i++){
-        var selected=salon.pets[1];
-        if(selected.ide===petId){
+        var selected=salon.pets[i];
+        if(selected.id===petId){
             indexDelete=i;
             break;
         }
+        console.log(i);
     }
 
-    //delete the pet from the array
+//delete the pet from the array
     salon.pets.splice(indexDelete,1);
-    //delete the pet from the html
+//delete the pet from the html
     card.remove();
     updateProfits();
 }
 
 function searchPet(){
-// add the search input and search button
-// get the value from the input
-    var ss=$('#petSearch').val();
-//search the pet on the array
+    // add the search input and search button
+    // get the value from the input
+    var ss=$('#searchPet').val();
+    //search the pet on the array
     salon.pets.forEach(aPet=>{
         if(aPet.name.toLowerCase().includes(ss.toLowerCase())||
-        aPet.service.toLowerCase().includes(ss.toLowerCase())
-     ){
-         $('#'+aPet.id).removeClass('unactive');
-     }else{
-         $('#'+aPet.id).addClass('unactive');
-     }
- });
+           aPet.service.toLowerCase().includes(ss.toLowerCase())
+        ){
+            $('#'+aPet.id).removeClass('unactive');
+        }else{
+            $('#'+aPet.id).addClass('unactive');
+        }
+    });
 // change the css to display the result
 }
 
 //create  3 pets
-    var pet1= new Pet("Scooby",50,"Male","Dane","Brown","Grooming","Shaggy","555-555-5555");
-    var pet2= new Pet("Tiger",50,"Male","Mix","Brown","Full Service","Tim","555-555-5556");
-    var pet3= new Pet("Tweety Bird",70,"Male","Cannary","Yellow","Nail Cut","Bugs Bunny","555-555-5557");
+    var pet1 = new Pet("Scooby",50,"Male","Dog","Dane","Brown","Grooming","Shaggy","555-555-5555","shaggy@gmail.com");
+    var pet2 = new Pet("Tiger",50,"Male","Dog","Mixed","Brown","Full Service","Tim","555-555-5556","tiger@gmail.com");
+    var pet3 = new Pet("Tweety Bird",70,"Male","Bird","Yellow","Nail Cut","Bugs Bunny","555-555-5557","bb@gmail.com");
     salon.pets.push(pet1);
     salon.pets.push(pet2);
-    salon.pets.push(pet3);
-    
+    salon.pets.push(pet3)
 
 function init(){
     console.log("Executed");
 
 
-    //display table
-    displayTable(pet1);
-    displayTable(pet2);
-    displayTable(pet3);
+    displayTable(pet1),
+    displayTable(pet2),
+    displayTable(pet3),
     
-    displayTable();
-
+    display();
+    
+   
 //hook events
-$('#register-btn').click(register);
-$('#search-btn').on('click',searchPet);
-$('#ownerPhone').keypress(function(e){
-    console.log(e.key);
-    if(e.key==="Enter"){
-        register();
-    }
-});    
-$('#searchPet').on('keyup',searchPet);
+    $('#register-btn').click(register);
+    $('#search-btn').on('click',searchPet);
+    $('#ownerPhone').keypress(function(e){
+        console.log(e.key);
+        if(e.key==="Enter"){
+            register();
+        }
+    });    
+    $('#searchPet').on('keyup',searchPet);
 }
-
-window.onload=init;

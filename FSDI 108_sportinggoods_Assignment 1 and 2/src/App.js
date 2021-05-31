685cd23a06f4126e2ecfe78137043a0081bd7681
Thad Siwecki <80732159+tsiwecki@users.noreply.github.com>
2021-05-31 17:13:19 -0500

    Add files via upload

---
+++ FSDI 108_sportinggoods_Assignment 1 and 2/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import Footer from "./components/footer";
+import NavBar from "./components/navBar";
+import Catalog from "./components/catalog"
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <h1>Sporting Goods</h1>
+      
+     
+      <Catalog></Catalog>
+
+      
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
+
+
